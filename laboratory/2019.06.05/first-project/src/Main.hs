a43d53b5441a5360cf52ebbc3d3151ae02b06d10
Alessandro Jean <alessandrojean@gmail.com>
2019-06-05 19:32:15 -0300

    Fix typo.

--- laboratory/2019.06.05/first-project/src/Main.hs
+++ laboratory/2019.06.05/first-project/src/Main.hs
@@ -2,13 +2,13 @@ module Main where
 
 triple x = x * 3
 
-finalAverage activitie test project = 10.0 / (c1 + c2 + c3)
+harmonicMean activity test project = 10.0 / (c1 + c2 + c3)
     where
-      c1 = 4.0 / max 0.1 activitie
+      c1 = 4.0 / max 0.1 activity
       c2 = 3.0 / max 0.1 test
       c3 = 3.0 / max 0.1 project
 
 main :: IO ()
 main = do
   print (triple 2)
-  print (finalAverage 10 7 8)
+  print (harmonicMean 10 7 8)
